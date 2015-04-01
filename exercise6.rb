6aa5df98049203c9c9d6ec62fc813a3fdb7ae868
Shem <shemca@gmail.com>
2015-04-01 15:31:30 -0400

    Second item display

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,23 @@
-grocery_list = ["tomatoes", "pizza", "grapes", "lemons"]
+list = ["tomatoes", "pizza", "grapes", "lemons"]
+
+def groceries(grocery_list)
 
 grocery_list.each do |n| 
 	puts "*#{n}"
 end
+end
+groceries(list)
+
+list << "rice"
+
+groceries(list)
+
+puts "The amount of items are #{list.length}"
+
+if list.include? "Bananas"
+	puts "You don't need to pick up bananas today."
+else
+	puts "You need to pick up bananas."
+end
+
+puts list[1]
